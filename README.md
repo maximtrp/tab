2a2db552eaad77b9f08549bdf4f85a634e21be80
maximtrp <maximtrp@gmail.com>
2018-08-03 15:19:59 +0300

    Init

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # tab
-Start page generator written in Python
+
+`tab` is a simple start page generator written in Python. It uses primitive templating, YAML, and outputs a single `index.html`. Default template is based on [Semantic UI](https://semantic-ui.com/).
+
+## Basics
+
+Bookmarks are stored in a separate YAML file names `bookmarks.yml`. Its format should fit the following one:
+
+```
+Coding:
+    GitHub:
+        href: //github.com
+    Travis CI:
+        href: //travis-ci.org
+
+Learn:
+    Stepik:
+        href: //stepik.org
+    Udacity:
+        href: //udacity.com
+    KhanAcademy:
+        href: //khanacademy.org
+```
+
+This format is simple and intuitive. `Coding` is a group name (non-indeted block), `GitHub` is an entry (link) name, and `href` is used to store an URL (`tab` does not process it in any way now, so it is up to you).
+
+Template files are currently hardcoded to be placed in a `template` dir. There must be `index.html`, `group.html`, `link.html` files.
+
+Stylesheet `styles.css` is optional. It may be integrated into `index.html`(use `-s` flag).
+
+By default, group and link icons are enabled. To disable, use `-r` flag.
+
+## Template
+
+You may create your own template. Just be sure to use these template variables:
+
+* `{styles}` is for an integrated stylesheet.
+* `{styles_ext}` is for an external stylesheet.
+* `{groups}` — wrapper placeholder for all groups html.
+* `{groupname}` — name of a group.
+* `{links}` — placeholder for links.
+* `{title}` — link title.
+* `{href}` — link href.
+
+## Usage
+
+Run with a `-h` flag to see help.
+
+```
+$ python tab.py -h
+Usage: tab.py [options]
+
+Options:
+  -h, --help  show this help message and exit
+  -s, --css   integrate "template/styles.css" into index.html
+  -r          remove icons
+```
+
+## Example
+
+There is an example `index.html` in `output` dir.
---
+++ tab.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+import os
+import yaml
+import sys
+from functools import reduce
+from optparse import OptionParser
+
+
+# Command-line options
+parser = OptionParser(usage="Usage: %prog [options]")
+parser.add_option("-s", "--css", dest="styles", action="store_true", help='integrate "template/styles.css" into index.html', default=False)
+parser.add_option("-r", dest="remove_icons", action="store_true", help="remove icons", default=False)
+options, args = parser.parse_args()
+
+if not options.remove_icons:
+    icons = {'group': '<i class="folder open"></i> ', 'entry': '<i class="linkify"></i> '}
+
+if options.styles:
+    f = open('template/styles.css', 'r')
+    styles = f.read()
+    f.close()
+
+# Importing bookmarks
+bm = open('bookmarks.yml')
+bm_yaml = yaml.safe_load(bm)
+bm.close()
+
+
+# Importing template files
+with open('template/index.html') as f:
+    body_tmp = f.read()
+with open('template/group.html') as f:
+    group_tmp = f.read()
+with open('template/link.html') as f:
+    link_tmp = f.read()
+
+
+# Iterating YAML
+# bm_yaml is a list of dicts
+groups_prehtml = []
+
+for group_title, entries in bm_yaml.items():
+
+    # Group contains title and entries
+    # Iterating a group
+    entries_prehtml = []
+    
+    # Iterating over entries and attributes
+    for entry_title, entry_info in entries.items():
+        if not options.remove_icons:
+            entry_title = icons['entry'] + entry_title
+        rep = ('{title}', entry_title), ('{href}', entry_info['href'])
+        entry_str = reduce(lambda a, k: a.replace(*k), rep, link_tmp.strip())
+        entries_prehtml.append(entry_str)
+
+    entries_html = " ".join(entries_prehtml)
+    if not options.remove_icons:
+        group_title = icons['group'] + group_title
+    rep = ('{groupname}', group_title), ('{links}', entries_html)
+    group_str = reduce(lambda a, v: a.replace(*v), rep, group_tmp)
+    groups_prehtml.append(group_str)
+
+
+# Making groups
+group_html = "\n".join(groups_prehtml)
+num_words = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six'}
+num = min(len(bm_yaml), 6)
+rep = ('{groups}', group_html), ('{num}', num_words[num]),\
+        ('{styles}', styles if options.styles else ''),\
+        ('{styles_ext}', '' if options.styles else '<link rel="stylesheet" type="text/css" href="styles.css">')
+body_html = reduce(lambda a, v: a.replace(*v), rep, body_tmp)
+
+
+# Creating an output dir
+os.makedirs('output', exist_ok=True)
+
+
+# Writing a file
+f = open('output/index.html', 'w')
+f.write(body_html)
+f.close()

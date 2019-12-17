5d80a6a0c01e061a80dfd54cbc646c217d74c5f0
maximtrp <maximtrp@gmail.com>
2019-12-17 20:38:19 +0300

    code style upd

--- tab.py
+++ tab.py
@@ -8,13 +8,19 @@ from optparse import OptionParser
 
 # Command-line options
 parser = OptionParser(usage="Usage: %prog [options]")
-parser.add_option("-c", dest="columns_num", type="int", metavar='NUM', help='maximum columns number', default=6)
-parser.add_option("-s", "--css", dest="styles", action="store_true", help='integrate "template/styles.css" into index.html', default=False)
-parser.add_option("-r", dest="remove_icons", action="store_true", help="remove icons", default=False)
+parser.add_option("-c", dest="columns_num", type="int",
+                  metavar='NUM', help='maximum columns number', default=6)
+parser.add_option("-s", "--css", dest="styles", action="store_true",
+                  help='integrate "template/styles.css" into index.html',
+                  default=False)
+parser.add_option("-r", dest="remove_icons", action="store_true",
+                  help="remove icons", default=False)
 options, args = parser.parse_args()
+
 
 def iconify(icon_name):
     return '<i class="' + icon_name + ' icon"></i>'
+
 
 if not options.remove_icons:
     icons = {'group': 'folder open', 'entry': 'linkify'}
@@ -53,7 +59,8 @@ for group_title, entries in bm_yaml.items():
     for entry_title, entry_info in entries.items():
         # Icons
         if not options.remove_icons:
-            icon = icons['entry'] if 'icon' not in entry_info else entry_info['icon']
+            icon = icons['entry'] if 'icon' not in entry_info\
+                                  else entry_info['icon']
             entry_title = iconify(icon) + entry_title
 
         # Links HTML
@@ -77,8 +84,10 @@ group_html = "\n".join(groups_prehtml)
 num_words = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six'}
 num = min(len(bm_yaml), options.columns_num)
 rep = ('{groups}', group_html), ('{num}', num_words[num]),\
-        ('{styles}', styles if options.styles else ''),\
-        ('{styles_ext}', '' if options.styles else '<link rel="stylesheet" type="text/css" href="styles.css">')
+    ('{styles}', styles if options.styles else ''),\
+    ('{styles_ext}',
+        '' if options.styles else
+        '<link rel="stylesheet" type="text/css" href="styles.css">')
 body_html = reduce(lambda a, v: a.replace(*v), rep, body_tmp)
 
 
